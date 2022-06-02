37e59b80d3d4df3761bbe0ef6f514d9e7c304222
Stefani <73163564+stefikh@users.noreply.github.com>
2022-06-02 16:13:37 +0300

    Update code_part_tried_to_automise.py

--- code/code_part_tried_to_automise.py
+++ code/code_part_tried_to_automise.py
@@ -1,17 +1,9 @@
-import os
 import json
 import re
-from natasha import Segmenter, NewsEmbedding, NewsNERTagger, Doc, MorphVocab
-
-segmenter = Segmenter()
-emb = NewsEmbedding()
-ner_tagger = NewsNERTagger(emb)
-morph_vocab = MorphVocab()
+import os
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'Covid_dict.json')
-
-#часть кода, где выкачиваем локации и записываем в словарь формата: "2020-01-22T14:33:33": ["Китае", "Ухань", "Хубэй", "США", "Таиланда", "Южной Кореи"]
+filename = os.path.join(dirname, 'Country_and_message1 not clean.json')
 
 sample_json = ''
 
@@ -19,18 +11,21 @@ with open(filename, "r", encoding="utf-8") as file:
   sample_json+=file.read()
 
 glossary = json.loads(sample_json)
-dict_local = {}
+dict_messages = {}
 for message in glossary:
-   locals = []
-   doc = Doc(glossary[message])
-   text = glossary[message]
-   doc.segment(segmenter)
-   doc.tag_ner(ner_tagger)
-   for span in doc.spans:
-          span.normalize(morph_vocab)
-          if span.type == 'LOC':
-              locals.append(span.normal)
-          dict_local[message] = locals, glossary[message]
-with open ('Country_and_message.json', 'w', encoding="utf-8") as file:
-    json.dump(dict_local, file, ensure_ascii=False)
+    glavnoe_message = glossary[message][1].split("',")
+    for element in glavnoe_message:
+        list_messages = []
+        if 'text' in element:
+            malenkoe_message = element.split("\\n")
+            for new in malenkoe_message:
+                text = str(new)
+                if re.findall(r'[Cc]ovid|COVID|[Кк]оронавирус[а-я]*|[Оо]чаг|[Зз]аражен[а-я]|[Пп]андем[а-я]|[Ээ]пидеми[а-я]|[Вв]ирус[а-я]*', text):
+                    text = re.sub(r"'},",'', text)
+                    text = re.sub(r"'text':", '', text)
+                    list_messages.append(text)
+        if len(list_messages) != 0:
+            dict_messages[message] = glossary[message][0], list_messages
+with open ('Country_and_message1 clean.json', 'w', encoding="utf-8") as file:
+    json.dump(dict_messages, file, ensure_ascii=False)
 
