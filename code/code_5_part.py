e3c1ac89ee9ae7cf815ab96a1f3d08c0a1300981
Stefani <73163564+stefikh@users.noreply.github.com>
2022-06-02 16:09:37 +0300

    Update code_5_part.py

--- code/code_5_part.py
+++ code/code_5_part.py
@@ -1,36 +1,35 @@
+#Удаляем из файла сам текст сообщений и проверяем, что ничего не потерялось
 import os
 import json
-import re
-from natasha import Segmenter, NewsEmbedding, NewsNERTagger, Doc, MorphVocab
-
-segmenter = Segmenter()
-emb = NewsEmbedding()
-ner_tagger = NewsNERTagger(emb)
-morph_vocab = MorphVocab()
-
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'Covid_dict.json')
-
-#часть кода, где выкачиваем локации и записываем в словарь формата: "2020-01-22T14:33:33": ["Китае", "Ухань", "Хубэй", "США", "Таиланда", "Южной Кореи"]
+filename = os.path.join(dirname, 'Country_and_coord_and_dynFULL.json')
 
 sample_json = ''
-
 with open(filename, "r", encoding="utf-8") as file:
-  sample_json+=file.read()
+    sample_json += file.read()
 
 glossary = json.loads(sample_json)
-dict_local = {}
-for message in glossary:
-   locals = []
-   doc = Doc(glossary[message])
-   text = glossary[message]
-   doc.segment(segmenter)
-   doc.tag_ner(ner_tagger)
-   for span in doc.spans:
-          span.normalize(morph_vocab)
-          if span.type == 'LOC':
-              locals.append(span.normal)
-          dict_local[message] = locals, glossary[message]
-with open ('Country_and_message.json', 'w', encoding="utf-8") as file:
-    json.dump(dict_local, file, ensure_ascii=False)
 
+new_glossary = {}
+
+glossary_countries_count = 0
+new_glossary_countries_count = 0
+
+for date in glossary:
+    countries = glossary[date][0]
+    for country in countries:
+        glossary_countries_count += 1
+
+for date in glossary:
+    new_glossary[date] = glossary[date][0]
+    
+for date in new_glossary:
+    countries = new_glossary[date]
+    for country in countries:
+        new_glossary_countries_count += 1
+        
+print(len(glossary), glossary_countries_count)
+print(len(new_glossary), new_glossary_countries_count)
+
+with open ('Country_and_coord_and_dynFULL_no_text.json', 'w', encoding="utf-8") as file:
+    json.dump(new_glossary, file, ensure_ascii=False)
