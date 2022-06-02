1944efbd7efb77cbebdb808836f917080fa1c47e
Stefani <73163564+stefikh@users.noreply.github.com>
2022-06-02 15:54:43 +0300

    Update code_1_part.py

--- code/code_1_part.py
+++ code/code_1_part.py
@@ -18,27 +18,3 @@ for elem in glossary:
         covid_dict[elem] = text
 with open ('Covid_dict.json', 'w', encoding="utf-8") as file:
     json.dump(covid_dict, file, ensure_ascii=False)
-
-#часть кода, где выкачиваем локации и записываем в словарь формата: "2020-01-22T14:33:33": ["Китае", "Ухань", "Хубэй", "США", "Таиланда", "Южной Кореи"]
-
-sample_json = ''
-
-with open("Covid_dict.json", "r", encoding="utf-8") as file:
-  sample_json+=file.read()
-
-glossary = json.loads(sample_json)
-dict_local = {}
-for message in glossary:
-   locals = []
-   doc = Doc(glossary[message])
-   text = glossary[message]
-   doc.segment(segmenter)
-   doc.tag_ner(ner_tagger)
-   for span in doc.spans:
-          span.normalize(morph_vocab)
-          if span.type == 'LOC':
-              locals.append(span.normal)
-          dict_local[message] = locals, glossary[message]
-with open ('Country_and_message.json', 'w', encoding="utf-8") as file:
-    json.dump(dict_local, file, ensure_ascii=False)
-
